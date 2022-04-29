33e2aac295304b2f483a45e4b51ae0447181a111
Nur Uddin <nuruddin46bd@gmail.com>
2022-04-30 00:40:56 +0600

    component set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./smartEx warehouse/Shared/Header/Header";
+import Blogs from "./smartEx warehouse/Blogs/Blogs";
+import Footer from "./smartEx warehouse/Shared/Footer/Footer";
+import Home from "./smartEx warehouse/Homepage/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World</h2>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<Blogs></Blogs>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
